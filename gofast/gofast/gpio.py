8a7810c61322c05e704cdf1d37f28b08220a1d05
Charles Goddard <Charles.Goddard@students.olin.edu>
2013-09-09 14:35:05 -0400

    Make gofast a proper module

---
+++ gofast/gofast/gpio.py
@@ -0,0 +1,42 @@
+
+import cffi
+
+ffi = cffi.FFI()
+
+ffi.cdef("""
+int setup(void);
+void setup_gpio(int gpio, int direction, int pud);
+int gpio_function(int gpio);
+void output_gpio(int gpio, int value);
+int input_gpio(int gpio);
+void set_rising_event(int gpio, int enable);
+void set_falling_event(int gpio, int enable);
+void set_high_event(int gpio, int enable);
+void set_low_event(int gpio, int enable);
+int eventdetected(int gpio);
+void cleanup(void);
+    """)
+C = ffi.verify(sources=['../c/c_gpio.c'], ext_package='gofast', modulename='_gpio')
+
+write = C.output_gpio
+read = C.input_gpio
+cleanup = C.cleanup
+setup = C.setup_gpio
+
+if C.setup():
+    def error(*args):
+        raise RuntimeError("Error initializing GPIO")
+    write, read, cleanup, setup = error, error, error, error
+
+INPUT = 1
+OUTPUT = 0
+
+HIGH = 1
+LOW = 0
+
+PUD_OFF  = 0
+PUD_DOWN = 1
+PUD_UP   = 2
+
+import atexit
+atexit.register(cleanup)
--- gofast/setup.py
+++ gofast/setup.py
@@ -5,4 +5,6 @@ import gpio
 setup(name='gofast',
     author='Charles Goddard',
     author_email='chargoddard@gmail.com',
+    packages=['gofast'],
+    ext_package='gofast',
     ext_modules=[gpio.ffi.verifier.get_extension()])
