1c02eda1e016e1e0a4e55443089617a65d608912
Charles Goddard <Charles.Goddard@students.olin.edu>
2013-09-09 14:47:51 -0400

    Fix setup.py

--- gofast/setup.py
+++ gofast/setup.py
@@ -1,10 +1,10 @@
 from distutils.core import setup
 
-import gpio
+import gofast.gpio
 
 setup(name='gofast',
     author='Charles Goddard',
     author_email='chargoddard@gmail.com',
     packages=['gofast'],
     ext_package='gofast',
-    ext_modules=[gpio.ffi.verifier.get_extension()])
+    ext_modules=[gofast.gpio.ffi.verifier.get_extension()])
